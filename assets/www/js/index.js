fd09b2b7f573d883e09e23237c5d2525aac045fc
yauri <yauri@live.com>
2013-03-30 21:35:51 +0800

    get current location and load google map

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,41 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
+    currentPosition : "",
+    map : "",
+
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -45,5 +23,47 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    loadMap: function() {
+        navigator.geolocation.getCurrentPosition(app.locationFound, app.locationError, { maximumAge: 3000, timeout: 15000, enableHighAccuracy: true });
+    },
+
+    locationFound: function(position) {
+        app.drawMap(position);
+    },
+
+    locationError: function() {
+        alert("Sorry cannot find your current location");
+    },
+
+    drawMap: function(position){
+
+        this.currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var mapOptions = {
+            center: this.currentPosition,
+            zoom: 15,
+            disableDefaultUI: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControl: true
+        };
+        this.map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        app.setCurrentPositionMarker(this.currentPosition);
+    },
+
+    setCurrentPositionMarker: function(currentPosition) {
+        var img = {
+            url: 'img/curPosMarker',
+            size: new google.maps.Size(50,50)
+        };
+
+        new google.maps.Marker({
+            position: currentPosition,
+            title: 'Current Location',
+            map: this.map,
+            optimized: false,
+            icon:img
+        });
     }
+
 };
